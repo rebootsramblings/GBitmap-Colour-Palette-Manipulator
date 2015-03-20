589d4b57a212a5ba8456c82af69735f7aa6abb36
Jonathan LaRocque <larocque.jonathan@gmail.com>
2015-03-20 17:22:43 -0400

    Removed build folder

--- build/c4che/_cache.py
+++
@@ -1,6 +0,0 @@
-BINDIR = '/usr/local/bin'
-DEFINES = ['RELEASE']
-LIBDIR = '/usr/local/lib'
-PEBBLE_SDK_COMMON = '/Users/jonathan/pebble-dev/PebbleSDK-3.0-dp3/Pebble/common'
-PREFIX = '/usr/local'
-TARGET_PLATFORMS = ['basalt', 'aplite']
