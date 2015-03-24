abf85bba8e5d077a0ef7bfe88e503f07377533d0
Jonathan LaRocque <larocque.jonathan@gmail.com>
2015-03-24 10:27:14 -0400

    Cleanup and efficiency
    Replaced if statement for APP_LOG calls with #ifdef to prevent
    compilation of this code for production apps. Cleaned up unnecessary
    comments.
    
    Fill all now accepts bool fill_gcolor clear to allow developer to
    decide if GColorClear should be replaced with the fill color.

--- src/gbitmap_color_palette_manipulator.c
+++ src/gbitmap_color_palette_manipulator.c
@@ -3,7 +3,7 @@
 
 #ifdef PBL_COLOR
 
-#define SHOW_APP_LOGS true
+#define SHOW_APP_LOGS
 
 char* get_gbitmapformat_text(GBitmapFormat format){
 	switch (format) {
@@ -39,29 +39,39 @@ void replace_gbitmap_color(GColor color_to_replace, GColor replace_with_color, G
 	//First determine what the number of colors in the palette
 	int num_palette_items = get_num_palette_colors(im);
 
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette has %d items", num_palette_items);
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette has %d items", num_palette_items);
+	#endif
 
 	//Get the gbitmap's current palette
 	GColor *current_palette = gbitmap_get_palette(im);
 
 	//Iterate through the palette finding the color we want to replace and replacing 
 	//it with the new color
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "--Replace Color Start--");
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "--Replace Color Start--");
+	#endif 
+
 	for(int i = 0; i < num_palette_items; i++){
 
-		if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette[%d] = %s", i, get_gcolor_text(current_palette[i]));
-
+		#ifdef SHOW_APP_LOGS
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette[%d] = %s (alpha:%d)", i, get_gcolor_text(current_palette[i]),(current_palette[i].argb >>6));
+		#endif
 
 		if ((color_to_replace.argb & 0x3F)==(current_palette[i].argb & 0x3F)){
 
 			current_palette[i].argb = (current_palette[i].argb & 0xC0)| (replace_with_color.argb & 0x3F);
-			if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "-------[%d] replaced with %s", i, get_gcolor_text(current_palette[i]) );
-
-			//break;//the color should only appear once in the palette
+			#ifdef SHOW_APP_LOGS
+			APP_LOG(APP_LOG_LEVEL_DEBUG, "-------[%d] replaced with %s (alpha:%d)", i, get_gcolor_text(current_palette[i]),(current_palette[i].argb >>6));
+			#endif
+			
 		}
 
 	}
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "--Replace Color End--");
+
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "--Replace Color End--");
+	#endif
 
 	//Mark the bitmaplayer dirty
 	if(bml != NULL){
@@ -70,29 +80,42 @@ void replace_gbitmap_color(GColor color_to_replace, GColor replace_with_color, G
 
 }
 
-void gbitmap_fill_all_except(GColor color_to_not_change, GColor fill_color, GBitmap *im, BitmapLayer *bml){
+void gbitmap_fill_all_except(GColor color_to_not_change, GColor fill_color, bool fill_gcolorclear, GBitmap *im, BitmapLayer *bml){
 
 	//First determine what the number of colors in the palette
 	int num_palette_items = get_num_palette_colors(im);
 
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette has %d items", num_palette_items);
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette has %d items", num_palette_items);
+	#endif
 
 	//Get the gbitmap's current palette
 	GColor *current_palette = gbitmap_get_palette(im);
 
 	//Iterate through the palette replacing all colors except the color_to_not_change
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "--Color Fill Start--");
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "--Color Fill Start--");
+	#endif
+
 	for(int i = 0; i < num_palette_items; i++){
 
-		if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette[%d] = %s", i, get_gcolor_text(current_palette[i]));
+		#ifdef SHOW_APP_LOGS
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Palette[%d] = %s", i, get_gcolor_text(current_palette[i]));
+		#endif
 
-		if(!GColorEq(color_to_not_change, current_palette[i])){
-			current_palette[i] = fill_color;
-			if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "-------[%d] filled with %s", i, get_gcolor_text(current_palette[i]) );
+		if(!GColorEq(color_to_not_change, current_palette[i])){//all colors except color_to_not_change
+			if((GColorEq(current_palette[i], GColorClear) && fill_gcolorclear) || !GColorEq(current_palette[i], GColorClear)){
+				current_palette[i] = fill_color;
+				#ifdef SHOW_APP_LOGS
+				APP_LOG(APP_LOG_LEVEL_DEBUG, "-------[%d] filled with %s", i, get_gcolor_text(current_palette[i]) );
+				#endif
+			}
 		}
 
 	}
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "--Color Fill End--");
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "--Color Fill End--");
+	#endif
 
 	//Mark the bitmap layer dirty
 	if(bml != NULL){
@@ -108,14 +131,19 @@ bool gbitmap_color_palette_contains_color(GColor m_color, GBitmap *im){
 	for(int i = 0; i < num_palette_items; i++){
 
 		if ((m_color.argb & 0x3F)==(current_palette[i].argb & 0x3F)){
-			if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "GBitmap contains: %s", get_gcolor_text(current_palette[i]));
+			#ifdef SHOW_APP_LOGS
+			APP_LOG(APP_LOG_LEVEL_DEBUG, "GBitmap contains: %s", get_gcolor_text(current_palette[i]));
+			#endif
 
 			return true;
 		}
 
 	}
 
-	if(SHOW_APP_LOGS) APP_LOG(APP_LOG_LEVEL_DEBUG, "GBitmap does not contain: %s", get_gcolor_text(m_color));
+	#ifdef SHOW_APP_LOGS
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "GBitmap does not contain: %s", get_gcolor_text(m_color));
+	#endif
+
 	return false;
 
 }
@@ -162,75 +190,6 @@ const char* get_gcolor_text(GColor m_color){
 	if(GColorEq(m_color, GColorClear))
 		return "GColorClear";
 	return GColorsNames[m_color.argb & 0x3F];
-/*
-	m_color.argb|=0xC0; //optional method
-	if(GColorEq(m_color, GColorBlack)){ return "GColorBlack";}
-	else if(GColorEq(m_color, GColorOxfordBlue)){ return "GColorOxfordBlue";}
-	else if(GColorEq(m_color, GColorDukeBlue)){ return "GColorDukeBlue";}
-	else if(GColorEq(m_color, GColorBlue)){ return "GColorBlue";}
-	else if(GColorEq(m_color, GColorDarkGreen)){ return "GColorDarkGreen";}
-	else if(GColorEq(m_color, GColorMidnightGreen)){ return "GColorMidnightGreen";}
-	else if(GColorEq(m_color, GColorCobaltBlue)){ return "GColorCobaltBlue";}
-	else if(GColorEq(m_color, GColorBlueMoon)){ return "GColorCobaltBlue";}
-	else if(GColorEq(m_color, GColorIslamicGreen)){ return "GColorIslamicGreen";}
-	else if(GColorEq(m_color, GColorJaegerGreen)){ return "GColorJaegerGreen";}
-	else if(GColorEq(m_color, GColorTiffanyBlue)){ return "GColorTiffanyBlue";}
-	else if(GColorEq(m_color, GColorVividCerulean)){ return "GColorVividCerulean";}
-	else if(GColorEq(m_color, GColorGreen)){ return "GColorGreen";}
-	else if(GColorEq(m_color, GColorMalachite)){ return "GColorMalachite";}
-	else if(GColorEq(m_color, GColorMediumSpringGreen)){ return "GColorMediumSpringGreen";}
-	else if(GColorEq(m_color, GColorCyan)){ return "GColorCyan";}
-	else if(GColorEq(m_color, GColorBulgarianRose)){ return "GColorBulgarianRose";}
-	else if(GColorEq(m_color, GColorImperialPurple)){ return "GColorImperialPurple";}
-	else if(GColorEq(m_color, GColorIndigo)){ return "GColorIndigo";}
-	else if(GColorEq(m_color, GColorElectricUltramarine)){ return "GColorElectricUltramarine";}
-	else if(GColorEq(m_color, GColorArmyGreen)){ return "GColorArmyGreen";}
-	else if(GColorEq(m_color, GColorDarkGray)){ return "GColorDarkGray";}
-	else if(GColorEq(m_color, GColorLiberty)){ return "GColorLiberty";}
-	else if(GColorEq(m_color, GColorVeryLightBlue)){ return "GColorVeryLightBlue";}
-	else if(GColorEq(m_color, GColorKellyGreen)){ return "GColorKellyGreen";}
-	else if(GColorEq(m_color, GColorMayGreen)){ return "GColorMayGreen";}
-	else if(GColorEq(m_color, GColorCadetBlue)){ return "GColorCadetBlue";}
-	else if(GColorEq(m_color, GColorPictonBlue)){ return "GColorPictonBlue";}
-	else if(GColorEq(m_color, GColorBrightGreen)){ return "GColorBrightGreen";}
-	else if(GColorEq(m_color, GColorScreaminGreen)){ return "GColorScreaminGreen";}
-	else if(GColorEq(m_color, GColorMediumAquamarine)){ return "GColorMediumAquamarine";}
-	else if(GColorEq(m_color, GColorElectricBlue)){ return "GColorElectricBlue";}
-	else if(GColorEq(m_color, GColorDarkCandyAppleRed)){ return "GColorDarkCandyAppleRed";}
-	else if(GColorEq(m_color, GColorJazzberryJam)){ return "GColorJazzberryJam";}
-	else if(GColorEq(m_color, GColorPurple)){ return "GColorPurple";}
-	else if(GColorEq(m_color, GColorVividViolet)){ return "GColorVividViolet";}
-	else if(GColorEq(m_color, GColorWindsorTan)){ return "GColorWindsorTan";}
-	else if(GColorEq(m_color, GColorRoseVale)){ return "GColorWindsorTan";}
-	else if(GColorEq(m_color, GColorPurpureus)){ return "GColorPurpureus";}
-	else if(GColorEq(m_color, GColorLavenderIndigo)){ return "GColorLavenderIndigo";}
-	else if(GColorEq(m_color, GColorLimerick)){ return "GColorLimerick";}
-	else if(GColorEq(m_color, GColorBrass)){ return "GColorBrass";}
-	else if(GColorEq(m_color, GColorLightGray)){ return "GColorLightGray";}
-	else if(GColorEq(m_color, GColorBabyBlueEyes)){ return "GColorBabyBlueEyes";}
-	else if(GColorEq(m_color, GColorSpringBud)){ return "GColorSpringBud";}
-	else if(GColorEq(m_color, GColorInchworm)){ return "GColorInchworm";}
-	else if(GColorEq(m_color, GColorMintGreen)){ return "GColorMintGreen";}
-	else if(GColorEq(m_color, GColorCeleste)){ return "GColorCeleste";}
-	else if(GColorEq(m_color, GColorRed)){ return "GColorRed";}
-	else if(GColorEq(m_color, GColorFolly)){ return "GColorFolly";}
-	else if(GColorEq(m_color, GColorFashionMagenta)){ return "GColorFashionMagenta";}
-	else if(GColorEq(m_color, GColorMagenta)){ return "GColorMagenta";}
-	else if(GColorEq(m_color, GColorOrange)){ return "GColorOrange";}
-	else if(GColorEq(m_color, GColorSunsetOrange)){ return "GColorSunsetOrange";}
-	else if(GColorEq(m_color, GColorBrilliantRose)){ return "GColorBrilliantRose";}
-	else if(GColorEq(m_color, GColorShockingPink)){ return "GColorShockingPink";}
-	else if(GColorEq(m_color, GColorChromeYellow)){ return "GColorChromeYellow";}
-	else if(GColorEq(m_color, GColorRajah)){ return "GColorRajah";}
-	else if(GColorEq(m_color, GColorMelon)){ return "GColorMelon";}
-	else if(GColorEq(m_color, GColorRichBrilliantLavender)){ return "GColorRichBrilliantLavender";}
-	else if(GColorEq(m_color, GColorYellow)){ return "GColorYellow";}
-	else if(GColorEq(m_color, GColorIcterine)){ return "GColorIcterine";}
-	else if(GColorEq(m_color, GColorPastelYellow)){ return "GColorPastelYellow";}
-	else if(GColorEq(m_color, GColorWhite)){ return "GColorWhite";}
-
-	return "UNKNOWN COLOR";
-*/
 
 }
 #endif	
